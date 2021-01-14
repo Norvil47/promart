75957edf6479e3272c4b4f8338e198a4f42e08fa
Norvil Stalin Guevara Torres <stalin.56.56.56@gmail.com>
2021-01-14 11:35:35 -0500

    agregar pruebas

---
+++ Persona.Test/ApplicationDnContextInMemory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Test
+{
+    public static class ApplicationDbContextInMemory
+    {
+        //public static ApplicationDbContext Get()
+        //{
+        //    var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+        //        .UseInMemoryDatabase(databaseName: $"Catalog.Db")
+        //        .Options;
+
+        //    return new ApplicationDbContext(options);
+        //}
+    }
+}
---
+++ Persona.Test/Persona/EliminarTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Test.Persona
+{
+    class EliminarTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void RegistrarUndocumentoQueExiste()
+        {
+            Assert.Pass();
+        }
+    }
+}
